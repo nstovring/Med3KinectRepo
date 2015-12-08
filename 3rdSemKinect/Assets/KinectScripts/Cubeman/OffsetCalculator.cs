5d5708a515429b9e84fada57d23d574384ed37bd
nstovring <nstovring@live.dk>
2015-12-08 13:34:37 +0100

    Added new shit!

---
+++ 3rdSemKinect/Assets/KinectScripts/Cubeman/OffsetCalculator.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System;
+
+public class OffsetCalculator : NetworkBehaviour {
+
+    private Vector3 player2Offset;
+    private float player2angleOffset;
+
+    private GameObject[] players;
+    private float player1AngleFromKinect;
+    [SyncVar] public Vector3 positionalOffset;
+    [SyncVar] public Vector3 rotationalOffset;
+
+    public  static OffsetCalculator offsetCalculator;
+
+	void Start ()
+	{
+	    offsetCalculator = this;
+	}
+
+
+	// Update is called once per frame
+    [Server]
+	void Update () {
+        /*players = GameObject.FindGameObjectsWithTag("Player");
+
+        if (this.players.Length >= 2) {
+            player2Offset = players[0].transform.position - players[1].transform.position;
+            player1AngleFromKinect = Mathf.Abs(players[0].transform.GetComponent<CubeController>().angleFromKinect);
+
+            CubeController player2Controller = players[1].transform.GetComponent<CubeController>();
+
+            player2angleOffset = player1AngleFromKinect + Mathf.Abs(player2Controller.angleFromKinect) + Mathf.Abs(player2Controller.angleBetweenKinects);
+            SetPositionOffset();
+            SetRotationOffset();
+        }*/
+	}
+
+    public void CalculateOffset()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+
+        /*if (this.players.Length >= 2)
+        {
+            player2Offset = GetPositionOffset();
+            player1AngleFromKinect = Mathf.Abs(players[0].transform.GetComponent<CubeController>().angleFromKinect);
+
+            CubeController player2Controller = players[1].transform.GetComponent<CubeController>();
+
+            player2angleOffset = player1AngleFromKinect + Mathf.Abs(player2Controller.angleFromKinect) + Mathf.Abs(player2Controller.angleBetweenKinects);
+        }*/
+    }
+
+    public Vector3 GetPositionOffset()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        positionalOffset = (players[0].transform.position - players[1].transform.position);
+        return positionalOffset;
+    }
+
+    public Vector3 GetRotationOffset()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        rotationalOffset = new Vector3(
+          Vector3.Angle(players[0].transform.right, players[1].transform.right),
+          Vector3.Angle(players[0].transform.up, players[1].transform.up),
+          Vector3.Angle(players[0].transform.forward, players[1].transform.forward));
+        return rotationalOffset;
+    }
+
+
+    [Server]
+    private void SetRotationOffset()
+    {
+        //Debug.Log(players[0].transform.forward  +" & "+ players[1].transform.forward);
+        //Debug.Log(Vector3.Angle(players[0].transform.forward, players[1].transform.forward));
+
+       // players[1].GetComponent<CubeController>().tempRotation = Vector3.Angle(players[0].transform.forward,
+          //players[1].transform.forward);
+    }
+    [Server]
+    private void SetPositionOffset()
+    {
+       // players[1].GetComponent<CubeController>().positionOffset = this.player2Offset;
+       // players[1].GetComponent<CubeController>().otherAngleFromKinect = this.player1AngleFromKinect;
+    }
+
+}
---
+++ 3rdSemKinect/Assets/KinectScripts/Cubeman/UserSyncPosition.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class UserSyncPosition : NetworkBehaviour
+{
+
+    [SyncVar] private Vector3 syncPos;
+    [SyncVar] private Vector3 syncRot;
+
+    [SyncVar] public bool Offset;
+    [SyncVar] private Color userColor;
+    [SyncVar] private string objectName;
+
+    [SerializeField] private Transform myTransform;
+    [SerializeField] private float lerpRate = 15;
+
+    public bool MirroredMovement;
+
+    private KinectManager manager;
+    private OffsetCalculator offsetCalculator;
+
+    // Update is called once per frame
+    [ClientCallback]
+    void FixedUpdate () {
+	    TransmitPosition();
+        LerpPosition();
+	}
+
+    void LerpPosition()
+    {
+        if (!isLocalPlayer)
+        {
+            myTransform.position = Vector3.Lerp(myTransform.position, syncPos, Time.deltaTime*lerpRate);
+            myTransform.rotation = Quaternion.Lerp(myTransform.rotation, Quaternion.Euler(syncRot), Time.deltaTime * lerpRate);
+            transform.GetComponent<MeshRenderer>().material.color = userColor;
+            transform.name = objectName;
+        }
+    }
+
+    [Command]
+    void CmdProvidePositionToServer(Vector3 pos, Vector3 rot)
+    {
+        syncPos = pos;
+        syncRot = rot;
+    }
+
+    [Command]
+    void Cmd_ChangeIdentity(Color col, string objectName)
+    {
+        this.objectName = objectName;
+        userColor = col;
+    }
+
+
+    void TransmitPosition()
+    {
+        if (isLocalPlayer)
+        {
+            if (manager == null)
+            {
+                manager = KinectManager.Instance;
+                
+            }
+            else
+            {
+                MoveWithUser();
+                CmdProvidePositionToServer(myTransform.position, myTransform.rotation.eulerAngles);
+            }
+        }
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        base.OnStartLocalPlayer();
+        string objectName = "User " + (GetComponent<NetworkIdentity>().netId.Value -1);
+        userColor = new Color(Random.value, Random.value, Random.value);
+        transform.GetComponent<MeshRenderer>().material.color = userColor;
+        transform.name = objectName;
+        Cmd_ChangeIdentity(userColor, objectName);
+    }
+
+    [Client]
+    private void MoveWithUser()
+    {
+        offsetCalculator = OffsetCalculator.offsetCalculator;
+        uint playerID = manager != null ? manager.GetPlayer1ID() : 0;
+        Vector3 posPointMan = manager.GetUserPosition(playerID);
+        posPointMan.z = !MirroredMovement ? -posPointMan.z : posPointMan.z;
+        posPointMan.x *= 1;
+        Quaternion direction = Quaternion.AngleAxis(offsetCalculator.rotationalOffset.y, Vector3.up);
+        transform.position = (direction*posPointMan) != Vector3.zero ? (direction*posPointMan) : posPointMan;
+        transform.position = posPointMan;
+        RotateWithUser();
+    }
+
+    [Client]
+    private void RotateWithUser()
+    {
+        if (manager.IsUserDetected())
+        {
+            uint userId = manager.GetPlayer1ID();
+
+            if (manager.IsJointTracked(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ShoulderLeft) &&
+                manager.IsJointTracked(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ShoulderRight))
+            {
+                Vector3 posLeftShoulder = manager.GetJointPosition(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ShoulderLeft);
+                Vector3 posRightShoulder = manager.GetJointPosition(userId, (int)KinectWrapper.NuiSkeletonPositionIndex.ShoulderRight);
+
+                posLeftShoulder.z = -posLeftShoulder.z;
+                posRightShoulder.z = -posRightShoulder.z;
+
+                Vector3 dirLeftRight = posRightShoulder - posLeftShoulder;
+                dirLeftRight -= Vector3.Project(dirLeftRight, Vector3.up);
+
+                Quaternion rotationShoulders = Quaternion.FromToRotation(Vector3.right, dirLeftRight);
+
+                myTransform.rotation = rotationShoulders;
+            }
+        }
+    }
+}
